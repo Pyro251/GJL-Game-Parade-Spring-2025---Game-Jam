407a036122be45e7fea8807de0eeef54961f4b59
Pyro251 <caidenmrichards@gmail.com>
2025-06-18 18:25:35 -0400

    Day 7.2
    Pretty much finished the game, but broke the shop a little bit. I hope to finalize and submit everything by tomorrow.

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -4,6 +4,19 @@ extends CharacterBody2D
 @onready var health_bar: ProgressBar = $Camera2D/HealthBar
 @onready var coins_label: Label = $Camera2D/Coins
 @onready var shop_ui: Node2D = $Camera2D/ShopUI
+@onready var attack_speed_timer: Timer = $AttackSpeed
+@onready var attack_speed_bar: ProgressBar = $Camera2D/AttackSpeedBar
+
+#card onready vars
+@onready var common_health: TextureButton = $Camera2D/ShopUI/CommonHealth
+@onready var rare_health: TextureButton = $Camera2D/ShopUI/RareHealth
+@onready var extraordinary_health: TextureButton = $Camera2D/ShopUI/ExtraordinaryHealth
+@onready var common_attack: TextureButton = $Camera2D/ShopUI/CommonAttack
+@onready var rare_attack: TextureButton = $Camera2D/ShopUI/RareAttack
+@onready var extraordinary_attack: TextureButton = $Camera2D/ShopUI/ExtraordinaryAttack
+@onready var common_attack_speed: TextureButton = $Camera2D/ShopUI/CommonAttackSpeed
+@onready var rare_attack_speed: TextureButton = $Camera2D/ShopUI/RareAttackSpeed
+@onready var extraordinary_attack_speed: TextureButton = $Camera2D/ShopUI/ExtraordinaryAttackSpeed
 
 @export var speed = 400
 
@@ -31,6 +44,21 @@ func _physics_process(delta):
 		shop_ui.hide()
 	if Global.hide_shop == false:
 		shop_ui.show()
+	
+	if Input.is_action_just_pressed("shop"):
+		if Global.hide_shop == false:
+			pick_shop_items()
+	
+	attack_speed_timer.wait_time = Global.attack_speed
+	attack_speed_bar.max_value = Global.attack_speed
+	attack_speed_bar.value = attack_speed_timer.time_left
+	
+	if Input.is_action_just_pressed("shoot"):
+		if Global.can_fire_bullet == true:
+			attack_speed_timer.start()
+	
+	if Global.player_health <= 0:
+		die()
 	
 	move_and_slide()
 
@@ -62,4 +90,105 @@ func _on_debug_check_timeout() -> void:
 		print("shop shown")
 
 func pick_shop_items():
-	var pick_health_card
+	var pick_health_card = rng.randi_range(1, 3)
+	var pick_attack_card = rng.randi_range(1, 3)
+	var pick_attack_speed_card = rng.randi_range(1, 3)
+	
+	#pick health card
+	if pick_health_card == 1:
+		common_health.show()
+		rare_health.hide()
+		extraordinary_health.hide()
+	if pick_health_card == 2:
+		common_health.hide()
+		rare_health.show()
+		extraordinary_health.hide()
+	if pick_health_card == 3:
+		common_health.hide()
+		rare_health.hide()
+		extraordinary_health.show()
+	
+	#pick attack card
+	if pick_attack_card == 1:
+		common_attack.show()
+		rare_attack.hide()
+		extraordinary_attack.hide()
+	if pick_attack_card == 2:
+		common_attack.hide()
+		rare_attack.show()
+		extraordinary_attack.hide()
+	if pick_attack_card == 3:
+		common_attack.hide()
+		rare_attack.hide()
+		extraordinary_attack.show()
+	
+	#pick attack speed card
+	if pick_attack_speed_card == 1:
+		common_attack_speed.show()
+		rare_attack_speed.hide()
+		extraordinary_attack_speed.hide()
+	if pick_attack_speed_card == 2:
+		common_attack_speed.hide()
+		rare_attack_speed.show()
+		extraordinary_attack_speed.hide()
+	if pick_attack_speed_card == 3:
+		common_attack_speed.hide()
+		rare_attack_speed.hide()
+		extraordinary_attack_speed.show()
+
+func _on_common_health_pressed() -> void:
+	if coins >= 4:
+		Global.max_player_health += 5
+		coins -= 4
+		Global.hide_shop = true
+
+func _on_rare_health_pressed() -> void:
+	if coins >= 12:
+		Global.max_player_health += 8
+		coins -= 12
+		Global.hide_shop = true
+
+func _on_extraordinary_health_pressed() -> void:
+	if coins >= 20:
+		Global.max_player_health += 12
+		coins -= 20
+		Global.hide_shop = true
+
+func _on_common_attack_pressed() -> void:
+	if coins >= 5:
+		Global.orb_damage += 1
+		coins -= 5
+		Global.hide_shop = true
+
+func _on_rare_attack_pressed() -> void:
+	if coins >= 10:
+		Global.orb_damage += 1.5
+		coins -= 10
+		Global.hide_shop = true
+
+func _on_extraordinary_attack_pressed() -> void:
+	if coins >= 18:
+		Global.orb_damage += 2
+		coins -= 18
+		Global.hide_shop = true
+
+func _on_common_attack_speed_pressed() -> void:
+	if coins <= 5:
+		Global.attack_speed -= 0.05
+		coins -= 5
+		Global.hide_shop = true
+
+func _on_rare_attack_speed_pressed() -> void:
+	if coins <= 14:
+		Global.attack_speed -= 0.1
+		coins -= 14
+		Global.hide_shop = true
+
+func _on_extraordinary_attack_speed_pressed() -> void:
+	if coins <= 22:
+		Global.attack_speed -= 0.2
+		coins -= 22
+		Global.hide_shop = true
+
+func die():
+	get_tree().change_scene_to_file("res://Scenes/Menus/death_screen.tscn")
--- Scripts/Player/staff.gd
+++ Scripts/Player/staff.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var firing_pos: Node2D = $FiringPos
+@onready var attack_speed_timer: Timer = $AttackSpeed
 
 var bullet_path = preload("res://Scenes/Players/orb.tscn")
 var bullet_speed : float = 1000
@@ -10,7 +11,10 @@ func _physics_process(delta: float) -> void:
 	look_at(get_global_mouse_position())
 	
 	if Input.is_action_just_pressed("shoot"):
-		fire()
+		if Global.can_fire_bullet == true:
+			fire()
+	
+	attack_speed_timer.wait_time = Global.attack_speed
 
 
 func fire():
@@ -20,3 +24,9 @@ func fire():
 	var direction = (mouse_position - global_position).normalized() * bullet_speed
 	bullet.velocity = direction
 	get_tree().get_root().call_deferred("add_child", bullet)
+	Global.can_fire_bullet = false
+	attack_speed_timer.start()
+
+
+func _on_attack_speed_timeout() -> void:
+	Global.can_fire_bullet = true
